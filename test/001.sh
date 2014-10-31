3a9bdcdea84c793040839a1943bfa3ae63749686
Stephane Alnet <stephane@shimaore.net>
2014-10-31 10:58:21 +0100

    Base test OK.

--- test/001.sh
+++ test/001.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
+NAME=test-001-couchdb
 
-docker kill test-001-couchdb || true
-docker run -d --net host --name test-001-couchdb shimaore/couchdb
+docker kill $NAME 2>/dev/null || true
+docker rm $NAME 2>/dev/null || true
+docker run -d --net host --name $NAME shimaore/couchdb
 sleep 3
 curl http://127.0.0.1:5984/
 curl http://127.0.0.1:5984/_users
-docker kill test-001-couchdb
+docker kill $NAME
