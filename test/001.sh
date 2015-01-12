eddf2ad28f60124b1c2a3315d538b84a11ef48be
Stephane Alnet <stephane@shimaore.net>
2015-01-12 15:21:54 +0100

    Simplify command; actually make it usable.
    The config chain is already correct:
    
        $ /opt/couchdb/bin/couchdb -c
        /opt/couchdb/etc/couchdb/default.ini
        /opt/couchdb/etc/couchdb/local.ini
    
    On the other hand, only binding to a loopback interface doesn't work.

--- test/001.sh
+++ test/001.sh
@@ -4,8 +4,8 @@ NAME=test-001-couchdb
 
 docker kill $NAME 2>/dev/null || true
 docker rm $NAME 2>/dev/null || true
-docker run -d --net host --name $NAME shimaore/couchdb
+docker run -d -p 127.0.0.1:45984:5984 --name $NAME shimaore/couchdb
 sleep 3
-curl http://127.0.0.1:5984/
-curl http://127.0.0.1:5984/_users
+curl http://127.0.0.1:45984/
+curl http://127.0.0.1:45984/_users
 docker kill $NAME
